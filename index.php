ee23602d5e4ad7b6d93f4b378b00ca2cfd31181c
Erzi Hutama Dwirama Putra <erzihutama@gmail.com>
2019-05-12 19:56:23 +0700

    fi

--- index.php
+++ index.php
@@ -1,191 +1,290 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Azure Blob and Cognitive</title>
-    <style>
-        /* Fonts */
-        @import url(https://fonts.googleapis.com/css?family=Open+Sans:400);
+    
+    <title>IDENTIFIKASI GAMBAR </title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-        /* fontawesome */
-        @import url(http://weloveiconfonts.com/api/?family=fontawesome);
-        [class*="fontawesome-"]:before {
-            font-family: 'FontAwesome', sans-serif;
-        }
 
-        /* Simple Reset */
-        * { margin: 0; padding: 0; box-sizing: border-box; }
 
-        /* body */
-        body {
-            background: #e9e9e9;
-            color: #5e5e5e;
-            font: 400 87.5%/1.5em 'Open Sans', sans-serif;
-        }
-
-        /* Form Layout */
-        .form-wrapper {
-            background: #fafafa;
-            margin: 3em auto;
-            padding: 0 1em;
-            max-width: 500px;
-            padding: 10px;
-        }
-        
-        .form-wrapper > img {
-            width: 100%;
-        }
-
-        .form-wrapper > p {
-            text-align: center;
-            padding: 9.5px;
-            margin: 0 0 10px;
-            font-size: 13px;
-            word-break: break-all;
-            word-wrap: break-word;
-            background-color: #f5f5f5;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-        }
-
-        h1 {
-            text-align: center;
-            padding: 1em 0;
-        }
-
-        form {
-            padding: 0 1.5em;
-        }
-
-        .form-item {
-            margin-bottom: 0.75em;
-            width: 100%;
-        }
-
-        .form-item input {
-            background: #fafafa;
-            border: none;
-            border-bottom: 2px solid #e9e9e9;
-            color: #666;
-            font-family: 'Open Sans', sans-serif;
-            font-size: 1em;
-            height: 50px;
-            transition: border-color 0.3s;
-            width: 100%;
-        }
-
-        .form-item input:focus {
-            border-bottom: 2px solid #c0c0c0;
-            outline: none;
-        }
-
-        .button-panel {
-            margin: 2em 0 0;
-            width: 100%;
-        }
-
-        .button-panel .button {
-            background: #f16272;
-            border: none;
-            color: #fff;
-            cursor: pointer;
-            height: 50px;
-            font-family: 'Open Sans', sans-serif;
-            font-size: 1.2em;
-            letter-spacing: 0.05em;
-            text-align: center;
-            text-transform: uppercase;
-            transition: background 0.3s ease-in-out;
-            width: 100%;
-        }
-
-        .button:hover {
-            background: #ee3e52;
-        }
-
-        .form-footer {
-            font-size: 1em;
-            padding: 2em 0;
-            text-align: center;
-        }
+       
     </style>
 </head>
-<body>
-    <div class="form-wrapper">
-        <h1>Azure Blob and Cognitive</h1>
-        <form action="index.php" method="POST" enctype="multipart/form-data">
-            <div class="form-item">
-                <label for="gambar">Pilih gambar</label>
-                <input type="file" name="gambar" id="gambar" required>
+
+<body class="text-center">
+
+ <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+           
+        </div>
+        </nav>
+    <script type="text/javascript">
+        function processImage(url) {
+            // **********************************************
+            // *** Update or verify the following values. ***
+            // **********************************************
+
+            // Replace <Subscription Key> with your valid subscription key.
+            var subscriptionKey = "c5e39121ac66495fb9a1d5e286789eab";
+
+            // You must use the same Azure region in your REST API method as you used to
+            // get your subscription keys. For example, if you got your subscription keys
+            // from the West US region, replace "westcentralus" in the URL
+            // below with "westus".
+            //
+            // Free trial subscription keys are generated in the "westus" region.
+            // If you use a free trial subscription key, you shouldn't need to change
+            // this region.
+            var uriBase =
+                "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+
+            // Request parameters.
+            var params = {
+                "visualFeatures": "Categories,Description,Color",
+                "details": "",
+                "language": "en",
+            };
+
+            // Display the image.
+            var sourceImageUrl = document.getElementById("inputImage").value;
+            console.log(sourceImageUrl)
+            document.querySelector("#sourceImage").src = url;
+
+            // Make the REST API call.
+            $.ajax({
+                    url: uriBase + "?" + $.param(params),
+
+                    // Request headers.
+                    beforeSend: function(xhrObj) {
+                        xhrObj.setRequestHeader("Content-Type", "application/json");
+                        xhrObj.setRequestHeader(
+                            "Ocp-Apim-Subscription-Key", subscriptionKey);
+                    },
+
+                    type: "POST",
+
+                    // Request body.
+                    data: '{"url": ' + '"' + url + '"}',
+                })
+
+                .done(function(data) {
+                    // Show formatted JSON on webpage.
+                    var a = JSON.stringify(data, null, 2);
+                    console.log(data.description.captions[0].text);
+                    console.log(data.description.tags);
+                    $("#responseTextArea").val(JSON.stringify(data, null, 2));
+                    $("#desc").html(JSON.stringify(data.description.captions[0].text, null, 2).toUpperCase());
+                    $("#tags").html("tags = " + JSON.stringify(data.description.tags, null, 2));
+                })
+
+                .fail(function(jqXHR, textStatus, errorThrown) {
+                    // Display error message.
+                    var errorString = (errorThrown === "") ? "Error. " :
+                        errorThrown + " (" + jqXHR.status + "): ";
+                    errorString += (jqXHR.responseText === "") ? "" :
+                        jQuery.parseJSON(jqXHR.responseText).message;
+                    alert(errorString);
+                });
+        };
+    </script>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-10">
+                <form class="form-signin" action="index.php" method="POST" enctype="multipart/form-data">
+                    <h1 class="h3 mb-3 font-weight-normal">Upload image untuk mengidentifikasi gambar</h1>
+                    <label for="inputImage" class="sr-only">Image</label>
+                    <input type="file" name="file" id="inputImage" class="form-control" placeholder="Image" required autofocus>
+                    <br>
+                    <button class="btn btn-lg btn-primary " type="submit">Submit</button>
+                </form>
             </div>
-            <div class="button-panel">
-                <input type="submit" value="Submit" class="button"/>
+            <div class="col-10">
+                <center>
+                    <img id="sourceImage" width="450" />
+                    <br>
+                    <div id="jsonOutput" style="display:table-cell;">
+                        <h2 id="desc"></h2>
+                        <p id="tags"></p>
+                    </div>
+                </center>
             </div>
-        </form>
-        <div class="form-footer"></div>
+        </div>
     </div>
+</body>
+
+</html>
 
 <?php
+/**----------------------------------------------------------------------------------
+ * Microsoft Developer & Platform Evangelism
+ *
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
+ * OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ *----------------------------------------------------------------------------------
+ * The example companies, organizations, products, domain names,
+ * e-mail addresses, logos, people, places, and events depicted
+ * herein are fictitious.  No association with any real company,
+ * organization, product, domain name, email address, logo, person,
+ * places, or events is intended or should be inferred.
+ *----------------------------------------------------------------------------------
+ **/
+
+/** -------------------------------------------------------------
+# Azure Storage Blob Sample - Demonstrate how to use the Blob Storage service. 
+# Blob storage stores unstructured data such as text, binary data, documents or media files. 
+# Blobs can be accessed from anywhere in the world via HTTP or HTTPS. 
+#
+# Documentation References: 
+#  - Associated Article - https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-php 
+#  - What is a Storage Account - http://azure.microsoft.com/en-us/documentation/articles/storage-whatis-account/ 
+#  - Getting Started with Blobs - https://azure.microsoft.com/en-us/documentation/articles/storage-php-how-to-use-blobs/
+#  - Blob Service Concepts - http://msdn.microsoft.com/en-us/library/dd179376.aspx 
+#  - Blob Service REST API - http://msdn.microsoft.com/en-us/library/dd135733.aspx 
+#  - Blob Service PHP API - https://github.com/Azure/azure-storage-php
+#  - Storage Emulator - http://azure.microsoft.com/en-us/documentation/articles/storage-use-emulator/ 
+#
+ **/
+
 require_once 'vendor/autoload.php';
+require_once "./random_string.php";
 
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('ACCOUNT_NAME').";AccountKey=".getenv('ACCOUNT_KEY');
-$status = false;
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=submision2fix;AccountKey=2i36fdoAJmVmm1onq/o0ZtNQaePOMAzcRRS8HlmZoxQGM6X7aOmfCVaYG4cGUMp8SV5XUXOvG7ktlCNO6s2GfQ==";
 
-if (isset($_FILES['gambar'])) {
-    $check = getimagesize($_FILES["gambar"]["tmp_name"]);
-    if ($check !== false) {
-        $filename = md5(date("Y-m-d H:i:s")).".".getExtension($_FILES['gambar']['name']);
-        $blobClient = BlobRestProxy::createBlobService($connectionString);
-        $blobClient->createBLockBlob("submission", $filename, fopen($_FILES["gambar"]["tmp_name"], "r"));
-        $status = true;
-        request($filename);
+// Create blob client.
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+
+if (isset($_FILES["file"])) {
+    $target_file = getcwd();
+    $a = str_replace('\\', '/', $target_file);
+    $b = $a . '/' . basename($_FILES["file"]["name"]);
+    $c = basename($_FILES["file"]["name"]);
+    $fileToUpload = $c;
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $b)) {
+        echo "<script type='text/javascript'>alert('The file $c has been uploaded.');</script>";
+        if (!isset($_GET["Cleanup"])) {
+            // Create container options object.
+            $createContainerOptions = new CreateContainerOptions();
+
+            // Set public access policy. Possible values are
+            // PublicAccessType::CONTAINER_AND_BLOBS and PublicAccessType::BLOBS_ONLY.
+            // CONTAINER_AND_BLOBS:
+            // Specifies full public read access for container and blob data.
+            // proxys can enumerate blobs within the container via anonymous
+            // request, but cannot enumerate containers within the storage account.
+            //
+            // BLOBS_ONLY:
+            // Specifies public read access for blobs. Blob data within this
+            // container can be read via anonymous request, but container data is not
+            // available. proxys cannot enumerate blobs within the container via
+            // anonymous request.
+            // If this value is not specified in the request, container data is
+            // private to the account owner.
+            $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+
+            // Set container metadata.
+            $createContainerOptions->addMetaData("key1", "value1");
+            $createContainerOptions->addMetaData("key2", "value2");
+
+            $containerName = "blockblobs" . generateRandomString();
+
+            try {
+                // Create container.
+                $blobClient->createContainer($containerName, $createContainerOptions);
+
+                // Getting local file so that we can upload it to Azure
+                $myfile = fopen($fileToUpload, "r") or die("Unable to open file!");
+                fclose($myfile);
+
+                # Upload file as a block blob
+                // echo "Uploading BlockBlob: " . PHP_EOL;
+                // echo $fileToUpload;
+                // echo "<br />";
+
+                $content = fopen($fileToUpload, "r");
+
+                //Upload blob
+                $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+
+                // List blobs.
+                $listBlobsOptions = new ListBlobsOptions();
+                $listBlobsOptions->setPrefix($c);
+
+                // echo "These are the blobs present in the container: ";
+
+                do {
+                    $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+                    foreach ($result->getBlobs() as $blob) {
+                        // echo $blob->getName() . ": " . $blob->getUrl() . "<br />";
+                        echo "<script type='text/javascript'>processImage('" . $blob->getUrl() . "');</script>";
+                    }
+                    $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+                } while ($result->getContinuationToken());
+                echo "<br />";
+
+                // Get blob.
+                // echo "This is the content of the blob uploaded: ";
+                // $blob = $blobClient->getBlob($containerName, $fileToUpload);
+                // fpassthru($blob->getContentStream());
+                // echo "<br />";
+            } catch (ServiceException $e) {
+                // Handle exception based on error codes and messages.
+                // Error codes and messages are here:
+                // http://msdn.microsoft.com/library/azure/dd179439.aspx
+                $code = $e->getCode();
+                $error_message = $e->getMessage();
+                echo $code . ": " . $error_message . "<br />";
+            } catch (InvalidArgumentTypeException $e) {
+                // Handle exception based on error codes and messages.
+                // Error codes and messages are here:
+                // http://msdn.microsoft.com/library/azure/dd179439.aspx
+                $code = $e->getCode();
+                $error_message = $e->getMessage();
+                echo $code . ": " . $error_message . "<br />";
+            }
+        } else {
+
+            try {
+                // Delete container.
+                echo "Deleting Container" . PHP_EOL;
+                echo $_GET["containerName"] . PHP_EOL;
+                echo "<br />";
+                $blobClient->deleteContainer($_GET["containerName"]);
+            } catch (ServiceException $e) {
+                // Handle exception based on error codes and messages.
+                // Error codes and messages are here:
+                // http://msdn.microsoft.com/library/azure/dd179439.aspx
+                $code = $e->getCode();
+                $error_message = $e->getMessage();
+                echo $code . ": " . $error_message . "<br />";
+            }
+        }
     } else {
-        $status = false;
+        echo "Sorry, there was an error uploading your file.";
     }
+    
 }
-
-function getExtension($filename) {
-    return explode(".", $filename)[count(explode(".", $filename))-1];
-}
-
-function request($filename) {
-    global $url;
-    global $caption;
-    $url = "https://rdsdicodingstorage.blob.core.windows.net/submission/".$filename;
-    $data = '{"url":"'.$url.'"}';
-    $headers = array();
-    $headers[] = "Content-Type: application/json";
-    $headers[] = "Ocp-Apim-Subscription-Key: d2b7f29dfe2c47fea8a1218b77c42f85";
-
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze    ");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-    $result = curl_exec($ch);
-    $json = json_decode($result);
-    if (curl_errno($ch)) {
-        echo 'Error:' . curl_error($ch);
-    }
-    curl_close ($ch);
-    $caption = $json->description->captions[0]->text;
-}
-
-if ($status):
 ?>
 
-<div class="form-wrapper">
-    <img src="<?= $url ?>">
-    <p><?= $caption ?></p>
-</div>
-<?php endif; ?>
-*kode style form: <a href="https://codepen.io/bowie/pen/erEoh">https://codepen.io/bowie/pen/erEoh</a><br>
-</body>
-</html>
